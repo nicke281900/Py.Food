1850008a53e3da1e1698bb5e7ed5228514f9a2c6
nicke281900 <126591579+nicke281900@users.noreply.github.com>
2025-02-14 12:27:03 -0300

    Add files via upload

---
+++ Py,Food/App.py
@@ -0,0 +1,18 @@
+print('PayFood\n')
+
+print('1. Cadastrar restaurante')
+print('2. Lista de restaurante')
+print('3. Ativar restalrante')
+print('4. Sair')
+
+opcao_escolhida = int(input('Escolha uma opcao: '))
+print(f'voce escolheu a opcao{opcao_escolhida}')
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurante')
+elif opcao_escolhida == 3:
+    print('Ativar resturante')
+else:
+    print('Encerrando o programa')
